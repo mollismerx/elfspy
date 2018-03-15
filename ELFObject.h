aeea06b0897327cfdbf75823ebd349d74ee2238e
Robin Nicholson <mollis.merx@gmail.com>
2018-03-15 19:58:59 +0100

    cleanup

--- ELFObject.h
+++ ELFObject.h
@@ -1,6 +1,8 @@
 #ifndef ELFSPY_ELFOBJECT_HXX
 #define ELFSPY_ELFOBJECT_HXX
 
+#include <string>
+
 namespace spy
 {
 
@@ -9,9 +11,6 @@ struct ELFObject
   const char* name_ = "no name";
   unsigned char* base_ = nullptr;
   size_t size_ = 0;
-  size_t symbols_ = 0;
-  size_t strings_ = 0;
-  size_t rela_plt_ = 0;
 };
 
 } // namespace spy
