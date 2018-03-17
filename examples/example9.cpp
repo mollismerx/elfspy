779bc4687b004dad634fcfaa6b52a65949be6686
Robin Nicholson <mollis.merx@gmail.com>
2018-03-17 09:04:45 +0100

    added asserts

--- examples/example9.cpp
+++ examples/example9.cpp
@@ -5,6 +5,8 @@
 #include "elfspy/Fake.h"
 #include "demo.h"
 
+bool g1_replaced = false;
+bool g2_replaced = false;
 void g1_replace(int n);
 void g2_replace();
 
@@ -17,16 +19,20 @@ int main(int argc, char** argv)
   auto g_fake = spy::fake(g_spy, &g2_replace);
   g(1);
   g();
+  assert(g1_replaced);
+  assert(g2_replaced);
   return 0;
 }
 
 void g1_replace(int n)
 {
+  g1_replaced = true;
   std::cout << "g1_replace(" << n << ")" << std::endl;
 }
 
 void g2_replace()
 {
+  g2_replaced = true;
   std::cout << "g2_replace()" << std::endl;
 }
 
